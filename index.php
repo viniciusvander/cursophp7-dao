e72a71e478382823da316eda12d5fe0fe83c92ce
Vinicius Vander <viniciusvander1@gmail.com>
2020-08-17 15:53:23 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -16,8 +16,21 @@ echo $root;*/
 //echo json_encode($search);
 
 //Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$");
+
+//echo $usuario;
+/*
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;*/
+
 $usuario = new Usuario();
-$usuario->login("root", "!@#$");
+
+$usuario->loadbyId(8);
+
+$usuario->update("professor", "!@#$%¨&*");
 
 echo $usuario;
 
